3ba3c6209ee734871b37d264ff9137e4645d96bb
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2025-09-08 13:06:54 -0400

    Initial arg Handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <string>
 
-int main() {
-    printf("METAR CLI\n");
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <airport_id>\n", argv[0]);
+        return 1;
+    }
+
+    std::string airport_id = "";
+    airport_id = argv[1];
+
+    if (airport_id.empty()) {
+        fprintf(stderr, "Error: airport_id required.\n");
+        return 1;
+    }
+
+    printf("Fetching METAR for %s...\n", airport_id.c_str());
 
     return 0;
 }
