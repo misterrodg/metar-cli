29c1bf365f450427510e6384f807cb5810b57041
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2026-03-18 21:18:40 -0400

    Add METAR Struct

---
+++ include/METAR.h
@@ -0,0 +1,65 @@
+#ifndef METAR_H
+#define METAR_H
+
+#include <optional>
+#include <string>
+#include <vector>
+
+struct CloudCoverage {
+    int altitude;
+    std::string coverage;
+    bool is_ceiling;
+};
+
+struct Pressure {
+    double pressure;
+    std::string unit;
+};
+
+struct ReportTime {
+    int day;
+    int hour;
+    int minute;
+};
+
+struct Variability {
+    int from_value;
+    int to_value;
+};
+
+struct Visibility {
+    double distance_sm;
+    bool less_than;
+    bool greater_or_equal;
+};
+
+struct Wind {
+    int direction;
+    int speed;
+    bool is_variable;
+    std::optional<int> gust;
+    std::optional<Variability> variability;
+};
+
+struct METAR {
+    std::string type;
+    std::string station_id;
+    std::optional<ReportTime> report_time;
+    std::string modifier;
+    std::optional<Wind> wind;
+    bool is_cavok;
+    std::optional<Visibility> visibility;
+    std::string rvr;
+    std::string weather;
+    bool is_clr;
+    bool is_skc;
+    bool is_nsc;
+    bool is_ncd;
+    std::vector<CloudCoverage> cloud_coverage;
+    std::optional<int> temperature;
+    std::optional<int> dewpoint;
+    std::optional<Pressure> pressure;
+    std::string remarks;
+};
+
+#endif
